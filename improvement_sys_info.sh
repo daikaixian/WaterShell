5a3baec0ed1c989570c91c351af84a55ccb3d0d1
Water <kaixian.dai@medishare.cn>
2017-03-21 09:42:31 +0800

    update sh

---
+++ improvement_sys_info.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+cat << _EOF_
+<html>
+<head>
+    <title>
+    The title of your page
+    </title>
+</head>
+
+<img src="">
+
+</img>
+<body>
+    Your page content goes here.
+</body>
+</html>
+_EOF_
---
+++ sys_info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# sysinfo_page - A script to produce an HTML file
+
+echo "<html>
+ <head>
+   <title>
+   The title of your page
+   </title>
+ </head>
+ 
+ <body>
+   Your page content goes here.
+ </body>
+</html>"
+
+
